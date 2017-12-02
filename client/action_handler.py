adc2a3c02639d756443ca8eb7e69a077b15fe9de
Bartosz Irzyk <bartosz.irzyk@miquido.com>
2017-12-02 11:13:42 +0100

    Action handler

---
+++ client/action_handler.py
@@ -0,0 +1,32 @@
+# from pynput.keyboard import Key, Controller
+# from pynput.mouse import Button, Controller
+import pynput
+
+class ActionHandler:
+    @staticmethod
+    def handleMouseEvent(event_dict):
+        # mouse = pynput.mouse.Controller()
+        # mouse.position = event_dict["position"]
+        # if event_dict["press"]:
+        #     if event_dict["press"] == "left":
+        #         mouse.press(pynput.mouse.Button.left)
+        #     else if event_dict["press"] == "right":
+        #         mouse.press(pynput.mouse.Button.right)
+        #     else if event_dict["press"] == "middle":
+        #         mouse.press(pynput.mouse.Button.middle)
+        # if event_dict["release"]:
+        #     if event_dict["release"] == "left":
+        #         mouse.press(pynput.mouse.Button.left)
+        #     else if event_dict["release"] == "right":
+        #         mouse.press(pynput.mouse.Button.right)
+        #     else if event_dict["release"] == "middle":
+        #         mouse.press(pynput.mouse.Button.middle)
+        # if event_dict["scroll"]:
+        #     mouse.scroll = event_dict["scroll"]
+        pass
+    
+    @staticmethod
+    def handleKeyboardEvent(event_dict):
+        pass
+       
+        
