9b512caeed5d3e632b3fda81946b6d0169dca335
Paweł Kazimierowicz <pawel@kazimierowicz.eu>
2017-12-02 09:31:07 +0100

    computer-ranger untested

---
+++ computer-ranger/index.js
@@ -0,0 +1,17 @@
+var noble = require('noble');
+
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    console.log('scanning...');
+    noble.startScanning([], true);
+  }
+  else {
+    noble.stopScanning();
+  }
+})
+
+noble.on('discover', function(peripheral) {
+    noble.stopScanning();
+    console.log('found peripheral:', peripheral.advertisement);
+}
