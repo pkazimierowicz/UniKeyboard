28a65ddbb36efbde64fc140a0fd125cdd6841836
Paweł Kazimierowicz <pawel@kazimierowicz.eu>
2017-12-02 11:22:10 +0100

    scanning our beacons

--- computer-ranger/ranger.js
+++ computer-ranger/ranger.js
@@ -1,11 +1,12 @@
 const noble = require('noble');
+const ourBeacons = [ "D0D3FA86CA7645EC9BD96AF43D4688EC", "D0D3FA86CA7645EC9BD96AF4BD825E79", "D0D3FA86CA7645EC9BD96AF4A17D1214", "D0D3FA86CA7645EC9BD96AF48B479BA7" ];
 
 let closest_beacon = null;
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     console.log('scanning...');
-    noble.startScanning([], true);
+    noble.startScanning(ourBeacons, true);
   }
   else {
     noble.stopScanning();
@@ -14,7 +15,7 @@ noble.on('stateChange', function(state) {
 
 noble.on('discover', function(peripheral) {
     //noble.stopScanning();
-    console.log('found peripheral:', peripheral.advertisement);
+    console.log('found peripheral:', peripheral);
 });
 
 module.export = () => { return closest_beacon; };
